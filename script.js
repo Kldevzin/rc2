97d2fecdf686ecc29ea477488a88889f3f69a246
Kldevzin <invesportff@gmail.com>
2026-09-23 16:37:37 +0000

    adição de foto, animação nas abas

---
+++ script.js
@@ -0,0 +1,8 @@
+
+    //   const links = document.querySelectorAll(".nav-link");
+
+    //   links.forEach(function (link) {
+    //     link.addEventListener("click", function () {
+    //       alert("Você clicou em: " + link.textContent);
+    //     })
+    //   });
